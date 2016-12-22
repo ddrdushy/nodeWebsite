3e1a35777e03d880cabb9d52459dc98f2c6c989c
ddrdushy <dushy2009@gmail.com>
2016-12-22 22:25:10 +0530

    installed bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static("node_modules/jquery/dist"));
 
 app.use('/', index);
 app.use('/users', users);
